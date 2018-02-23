cdb9710b7e45afbfe069ec55235107c4c8e95f4d
Mohamed Hariri Nokob <mthariri@gmail.com>
2018-02-22 22:39:12 -0800

    modified summarize2.py

--- coding_challenge/src/summarize2.py
+++ coding_challenge/src/summarize2.py
@@ -1,41 +1,38 @@
 import sys
 import datetime
 
-def updateRecord(Record,count,date,url):
+def updateRecord(Record,date,url):
     if date in Record:
         check = True
-        for j in range(1, count[date] + 1):
+        for j in Record[date]:
             if url in Record[date][j]:
                 check = False
                 Record[date][j].remove(url)
                 if j + 1 in Record[date]:
                     Record[date][j + 1].add(url)
                 else:
-                    count[date] += 1
                     Record[date][j + 1] = set([url])
                 break
         if check:
             Record[date][1].add(url)
     else:
         Record[date] = {1: set([url])}
-        count[date] = 1
 
 def summarizeFile(filepath):
     with open(filepath, 'r') as lines:
         Record = {}
-        count = {} # max count for each date
         for line in lines:
             if line.split():
                 timestamp, url = line.split('|')
                 date = datetime.datetime.utcfromtimestamp(int(timestamp)).date()
-                updateRecord(Record,count,date,url)
+                updateRecord(Record,date,url)
     dates = sorted(Record.keys())
     for date in dates:
         print(date.strftime("%m/%d/%Y")+" GMT")
-        rec = Record[date]
-        for j in range(count[date],0,-1):
-            for url in rec[j]:
-                print(url[:-1] + " " +str(j))
+        counts = sorted(Record[date].keys(),reverse=True)
+        for count in counts:
+            for url in Record[date][count]:
+                print(url[:-1] + " " +str(count))
 
 def main(filepath):
     """
